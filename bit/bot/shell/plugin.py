1378f061682bf124f6a49ddc0a05ad1d62ecae22
Ryan Northey <ryan@3ca.org.uk>
2012-03-20 21:10:16 +0000

    adding adapter for shell vars

--- bit/bot/shell/plugin.py
+++ bit/bot/shell/plugin.py
@@ -1,26 +1,2 @@
 
-from zope.interface import implements
-from zope.component import getUtility
-
-from twisted.manhole import telnet
-
-from twisted.application import internet
-
-from bit.core.interfaces import IPlugin, IApplication, IServices
-
-from bit.bot.base.plugin import BotPlugin
-
-class BotShell(BotPlugin):
-    implements(IPlugin)
-    name = 'bit.bot.shell'
-
-    @property
-    def services(self):
-        shell = telnet.ShellFactory()
-        shell.username = "x"
-        shell.password = "x"
-        shell.namespace['app'] = getUtility(IApplication)
-        shell.namespace['services'] = getUtility(IServices)
-        return {'shell': dict(args=[8081, shell]
-                                     ,service=internet.TCPServer)}
 
