4151b642c0eb1c6751aaa1ca03a0976770697182
Ryan Northey <ryan@3ca.org.uk>
2012-03-21 03:19:15 +0000

    adding shell var zcml

---
+++ bit/bot/shell/zcml.py
@@ -0,0 +1,32 @@
+
+import zope
+import os
+import bit
+
+from zope.i18nmessageid import MessageFactory
+_ = MessageFactory('bit.bot.shell')
+
+class IShellVarDirective(zope.interface.Interface):
+    """
+    Define a shell var
+    """
+    name = zope.schema.TextLine(
+        title=_("Name"), 
+        description=_("The shell var name"),       
+        required=True,
+        )
+    factory = zope.configuration.fields.GlobalObject(
+        title=_("Factory"),
+        description=_("The shell var factory"),       
+        required=True,
+        )
+
+def shell_var(_context, name, factory):
+    _context.action(
+        discriminator = None,
+        callable = zope.component.provideUtility,
+        args = (factory(),bit.bot.common.interfaces.IShellVar,name)
+        )
+
+        
+
