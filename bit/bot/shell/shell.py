00e1e0d7cb464ab21f8366cca9580e51d099b48f
Ryan Northey <ryan@3ca.org.uk>
2012-03-21 03:25:20 +0000

    pyflakes/pep8

--- bit/bot/shell/shell.py
+++ bit/bot/shell/shell.py
@@ -1,31 +1,31 @@
-
 from zope.interface import implementer
-
 from zope.component import getUtility, getAllUtilitiesRegisteredFor
+
+from twisted.manhole import telnet
 
 from bit.core.interfaces import IConfiguration, IApplication, IServices
 from bit.bot.common.interfaces import IShellVar
 
-from twisted.manhole import telnet
 
 def getShellPort():
-    return int(getUtility(IConfiguration).get('shell','port'))
+    return int(getUtility(IConfiguration).get('shell', 'port'))
+
 
 def getShell():
     print 'creating shell'
     shell = telnet.ShellFactory()
-    shell.username = getUtility(IConfiguration).get('shell','username')
-    shell.password = getUtility(IConfiguration).get('shell','password')
-    for k,v in getAllUtilitiesRegisteredFor(IShellVar):
+    shell.username = getUtility(IConfiguration).get('shell', 'username')
+    shell.password = getUtility(IConfiguration).get('shell', 'password')
+    for k, v in getAllUtilitiesRegisteredFor(IShellVar):
         shell.namespace[str(k)] = v
     return shell
+
 
 def shellVarApp():
     print 'setting shell app var'
     return getUtility(IApplication)
 
+
 @implementer(IShellVar)
 def shellVarServices():
     return getUtility(IServices)
-
-
